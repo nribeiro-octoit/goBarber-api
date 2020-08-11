c37eae8307bb6aeac1e245970db7111c43e30485
Nadson Dias Ribeiro <nadson.dr11@gmail.com>
2020-08-11 11:22:17 -0300

    Create Mail Template Provider and use handlebars

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -31,4 +31,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   });
 });
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log('⚡️ Server started on port 3333!');
+});
